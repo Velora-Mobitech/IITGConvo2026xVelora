1884d4a9e19458fc56c10a8dc5b5c8eee37fd0f9
tsm2503 <tattasrimanasa@gmail.com>
2026-06-24 00:21:49 +0530

    small ui changes

--- src/components/ConvocationInfo.tsx
+++ src/components/ConvocationInfo.tsx
@@ -71,7 +71,7 @@ export function ConvocationInfo() {
         </div>
 
         {/* Photo Opportunities */}
-        <motion.div
+        {/* <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
@@ -97,7 +97,7 @@ export function ConvocationInfo() {
               <p className="text-gray-600">Reconnect with batchmates and seniors at various events.</p>
             </div>
           </div>
-        </motion.div>
+        </motion.div> */}
       </div>
     </section>
   )
